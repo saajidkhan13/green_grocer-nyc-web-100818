16acf8d300fd70e9eed430eba9957f54cb060ea6
Saajid Khan <saajid.khan13@stjohns.edu>
2018-10-07 15:36:45 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,38 @@
-def consolidate_cart(cart)
-  # code here
+require 'pry'
+
+def consolidate_cart(cart:[])
+  result = {}
+  cart.each_with_index do |item, i|
+    item.each do |food, info|
+      if result[food]
+        result[food][:count] +=1 
+      else 
+        result[food] = info 
+        result[food][:count] = 1 
+      end 
+    end 
+  end 
+  result
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  coupon_cart = {} 
+  cart.each do |item, attributes|
+    coupons.each do |coupon|
+      if coupon[:item] == item && attributes[:count] >= coupon[:num] 
+        if !coupon_cart.has_key?("#{item} W/COUPON")
+          coupon_cart["#{item} W/COUPON"] = {
+            price: coupon[:cost],
+            clearance: attributes[:clearance],
+            count: 0 } 
+          end 
+          coupon_cart["#{item} W/COUPON"][:count] += 1 
+          attributes[:count] -= coupon[:num]
+        end
+      end
+    end 
+    cart.merge!(coupon_cart)
 end
 
 def apply_clearance(cart)
